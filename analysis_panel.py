0374ea9e3d040c5b2a7451cecfe7ef50e61cc0a9
Jamuna KC <Jamunakc738@gmail.com>
2023-08-25 11:06:16 +0530

    Project-Smart-Bin

--- analysis_panel.py
+++ analysis_panel.py
@@ -5,6 +5,13 @@ import plotly.express as px
 import firebase_admin
 from firebase_admin import credentials, db # for Firestore
 from dash import dcc, html, Input, Output
+from pushbullet import Pushbullet
+import numpy as np
+# your pushbullet's api-key
+apikey='your-api-key'
+pb=Pushbullet(apikey)
+# weekdays list
+days=['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 
 def determine_bar_color(value):
             if value > 30:
@@ -16,6 +23,12 @@ cred = credentials.Certificate('D:\jamu\Hackathon\smartbin-data-firebase-adminsd
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://smartbin-data-default-rtdb.firebaseio.com/'
 })
+# to obtain the fill duration
+def get_fillduration():
+    filldf=pd.read_csv("binfill.csv")
+    grouped = filldf.groupby(['DAY', 'S-ID'])['FILL-DURATION'].mean()
+    return grouped
+
 # running the dash app
 app = dash.Dash(__name__)
 
@@ -31,14 +44,22 @@ app.layout = html.Div([
                                     #if -1, then the interval has no limit (the default)
                                     #and if 0 then the interval stops running.
     ),
-
+    html.H4('Last updated on:'),
+    html.P(id='updated_time', style={'color': 'Blue', 'font-weight': 'bold'}),
     dcc.Graph(id="mybarchart"),
-
+    dcc.Dropdown(id="dropdown_day",
+                 options=[{'label': j, 'value': j} for j in days],
+                 multi=False,
+                 value='Mon',
+                 style={'width': "40%"}
+                 ),
+    dcc.Graph(id='fill-graph', figure={}),
 ])
 
 #------------------------------------------------------------------------
 @app.callback(
-    [Output('mybarchart', 'figure')],
+    Output('mybarchart', 'figure'),
+    Output(component_id='updated_time', component_property='children'),
     [Input('my_interval', 'n_intervals')]
 )
 def update_graph(num):
@@ -54,10 +75,33 @@ def update_graph(num):
         data_dict.append({'id':'S004','Distance':0.23,'Time':'203-08-14T09:26:08.085201'})
         data_dict.append({'id':'S005','Distance':10.3,'Time':'203-08-14T09:26:08.085201'})
         df=pd.DataFrame(data_dict)
+        uptime=data['Time']
         # plotting a bar graph
         fig=px.bar(df, x='id', y='Distance', title='Distance vs. ID',color='Distance',
                       color_discrete_map={value: determine_bar_color(value) for value in df['Distance']})
-        return [fig]
+        if data['Distance']<5:
+            push=pb.push_note('Smart-Bin filled!!','Dustbin S-001 is 90% filled')
+        return fig,uptime
+
+@app.callback(
+    Output('fill-graph', 'figure'),
+    [Input(component_id='dropdown_day', component_property='value')]
+
+)
+def update_graph(selected_day):
+    grouped=get_fillduration()
+    dictionary=dict(grouped[selected_day])
+    x_values = list(dictionary.keys())
+    y_values = list(dictionary.values())
+    y_values=np.divide(y_values,1440)                   # converting values in minutes to days
+    fig = px.bar(
+        x=x_values,
+        y=y_values,
+        title=f'FILL-DURATION on {selected_day}'
+    )
+    fig.update_xaxes(title_text='Smart-Bin_ID')
+    fig.update_yaxes(title_text='Number of Days')
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
